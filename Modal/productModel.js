9008aa7fe1d9ff6594f9ea3f70f969739cc214ad
Faizan Wani <fzwani@dc.iquasar.com>
2023-07-18 14:22:04 +0530

    initial commit

---
+++ Modal/productModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Product name is required"],
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Product = require("./Modal/productModel");
+
+dotenv.config({ path: "./config.env" });
+app.use(express.json());
+
+const port = 3000;
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.PASSWORD);
+
+app.get("/", (req, res) => {
+  res.send("home page");
+});
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log(`db connected`);
+    app.listen(port, () => {
+      console.log(`app is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+/// add products detail
+app.post("/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+/// to get product data
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// to get a unique product by id
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const products = await Product.findById(id);
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+/// update
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      res.status(404).send(`we cannot find the product bering id ${id}`);
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).send(updatedProduct);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+//delete a product
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      res.status(404).send(`we cannot find the product bering id ${id}`);
+    }
+
+    res.status(200).send(product);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
